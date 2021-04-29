f9e59ecfa3376bf33ea3cb0e545a86df852d1604
Eshwar Chockalingam <eshchock1@gmail.com>
2021-04-29 19:01:53 -0400

    swapped uniswap logo with bitswap

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,8 @@ import { useTranslation } from 'react-i18next'
 
 import styled from 'styled-components'
 
-import Logo from '../../assets/svg/logo.svg'
-import LogoDark from '../../assets/svg/logo_white.svg'
+import Logo from '../../assets/bitswap_small_white.png'
+import LogoDark from '../../assets/bitswap_small.png'
 import { useActiveWeb3React } from '../../hooks'
 import { useDarkModeManager } from '../../state/user/hooks'
 import { useETHBalances, useAggregateUniBalance } from '../../state/wallet/hooks'
